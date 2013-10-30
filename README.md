0aea930c1f419743c326b0ea409367bcdf837f9f
Geoff Hing <geoffhing@gmail.com>
2013-10-30 04:50:05 +0000

    Custom markers for hoop clusters

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Front end HTML/CSS/JavaScript for Hoops project.
+
+[Basketball Hoop](http://thenounproject.com/noun/basketball-hoop/#icon-No2711) icon designed by [Gabriele Fumero](http://thenounproject.com/Gabriele Fumero) from The Noun Project
--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,26 @@ require([
 ], function($, L) {
   var CHICAGO_CENTER = [41.881944, -87.627778];
 
+  var HoopIcon = L.Icon.extend({
+    options: {
+      iconUrl: 'assets/images/hoop-marker-icon.png',
+      shadowUrl: 'assets/images/hoop-marker-shadow.png',
+
+      iconSize:     [48, 38], // size of the icon
+      shadowSize:   [58, 28], // size of the shadow
+      iconAnchor:   [20, 16], // point of the icon which will correspond to marker's location
+      shadowAnchor: [17, 12],  // the same for the shadow
+      popupAnchor:  [0, -16] // point from which the popup should open relative to the iconAnchor
+    }
+  });
+  var hoopIcons = [new HoopIcon()];
+
+  for (var i = 1; i <= 9; i++) {
+    hoopIcons.push(new HoopIcon({
+      iconUrl: 'assets/images/hoop-marker-icon-' + i + '.png'
+    }));
+  }
+
   /**
    * Get HTML of a Leaflet popup from a hoop feature's properties
    */
@@ -23,7 +43,7 @@ require([
     var map = L.map('map').setView(CHICAGO_CENTER, 13);
     // add an OpenStreetMap tile layer
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors. <a href="http://thenounproject.com/noun/basketball-hoop/#icon-No2711" target="_blank">Basketball Hoop</a> designed by <a href="http://thenounproject.com/Gabriele Fumero" target="_blank">Gabriele Fumero</a> from The Noun Project'
     }).addTo(map);
 
     // BOOKMARK
@@ -32,6 +52,10 @@ require([
     // TODO: Basic styling of markers
     $.getJSON('data/basketball_courts.geojson', function(data) {
       L.geoJson(data, {
+        pointToLayer: function(feature, latlng) {
+          var numHoops = feature.properties.count || 0;
+          return L.marker(latlng, {icon: hoopIcons[numHoops]});
+        },
         onEachFeature: function (feature, layer) {
           layer.bindPopup(popupText(feature));
         }
